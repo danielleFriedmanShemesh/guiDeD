15bf6f0767e9b97fff95bd72f6ba8245b5b450a5
danielle <daniellefrsh@gmail.com>
2025-03-23 17:12:26 +0200

    סרגל כלים תחתון עובד

--- app/src/main/java/com/example/guided/Home_page.java
+++ app/src/main/java/com/example/guided/Home_page.java
@@ -1,14 +1,22 @@
 package com.example.guided;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,7 +28,7 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.guided.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Home_page extends AppCompatActivity implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
+public class Home_page extends AppCompatActivity implements View.OnClickListener, PopupMenu.OnMenuItemClickListener, BottomNavigationView.OnNavigationItemSelectedListener {
 
     ActivityMainBinding binding;
     ImageView menuBTN;
@@ -38,14 +46,16 @@ public class Home_page extends AppCompatActivity implements View.OnClickListener
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        replaceFragment(new HomeFragment());
+
 
         menuBTN = findViewById(R.id.option_btn);
         menuBTN.setOnClickListener(this);
 
-   //     navView = findViewById(R.id.bottomNavigationView);
-   //     navView.setItemIconTintList(null);
+        navView = findViewById(R.id.bottomNavigationView);
+        navView.setItemIconTintList(null);
 
-        replaceFragment(new HomeFragment());
+        navView.setOnNavigationItemSelectedListener(this);
 
 
 //        navView.setOnItemSelectedListener(item -> {
@@ -119,43 +129,90 @@ public class Home_page extends AppCompatActivity implements View.OnClickListener
         fragmentTransaction.commit();
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu){
-//        getMenuInflater().inflate(R.menu.menu_main,menu);
-//        //getMenuInflater().inflate(R.menu.toolbar_menu,menu);
-//        return true;
-//    }
-//       public boolean onOptionsItemSelected (MenuItem item){
-//           super.onOptionsItemSelected(item);
-//           int id= item.getItemId();
-//        if(id == R.id.action_profile){
-//            Toast.makeText(this, "action_profile", Toast.LENGTH_SHORT).show();
-//            return true;
-//        }
-//        else if(id ==  R.id.action_add_operation){
-//            Toast.makeText(this, "action_add_operation", Toast.LENGTH_SHORT).show();
-//            return true;
-//        }
-//        else if(id ==  R.id.action_add_trip){
-//            Toast.makeText(this, "action_add_trip", Toast.LENGTH_SHORT).show();
-//            return true;
-//        }
-//        else if(id ==  R.id.action_library_operations){
-//            Toast.makeText(this, "action_library_operations", Toast.LENGTH_SHORT).show();
-//            return true;
-//        }
-//        else if(id ==  R.id.action_library_trips){
-//            Toast.makeText(this, "action_library_trips", Toast.LENGTH_SHORT).show();
-//            return true;
-//        }
-//        else if(id ==  R.id.home){
-//            Toast.makeText(this, "action_home", Toast.LENGTH_SHORT).show();
-//            return true;
-//        }
-//        return false;
-//       }
-//
+        Intent intent;
+        int id= item.getItemId();
+        if(id == R.id.profile){
+            Toast.makeText(this, "action_profile", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if(id ==  R.id.add){
+            Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.bottom_sheet_layout);
+            TextView addOpTv = dialog.findViewById(R.id.op);
+            TextView addTrTv = dialog.findViewById(R.id.tr);
+            addTrTv.setText("הוספת טיול");
+            addOpTv.setText("הוספת פעולה");
+
+            LinearLayout addOP = dialog.findViewById(R.id.opLl);
+            addOP.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent;
+                    intent = new Intent(Home_page.this, Add_operation.class);
+                    startActivity(intent);
+                    dialog.dismiss();
+                }
+            });
+            LinearLayout addTR = dialog.findViewById(R.id.trLl);
+            addTR.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent;
+                    intent = new Intent(Home_page.this, Add_trip.class);
+                    startActivity(intent);
+                    dialog.dismiss();
+                }
+            });
+            dialog.show();
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.getWindow().setGravity(Gravity.BOTTOM);
+            return true;
+        }
+
+        else if(id ==  R.id.library){
+            Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.bottom_sheet_layout);
+            TextView libraryOpTv = dialog.findViewById(R.id.op);
+            TextView libraryTrTv = dialog.findViewById(R.id.tr);
+            libraryTrTv.setText("מאגר טיולים");
+            libraryOpTv.setText("מאגר פעולות");
+
+            LinearLayout libraryOP = dialog.findViewById(R.id.opLl);
+            libraryOP.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    replaceFragment(new LibraryOperationsFragment());
+                    dialog.dismiss();
+                }
+            });
+            LinearLayout libraryTR = dialog.findViewById(R.id.trLl);
+            libraryTR.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    replaceFragment(new LibraryTripsFragment());
+                    dialog.dismiss();
+                }
+            });
+            dialog.show();
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+            return true;
+        }
+
+        else if(id ==  R.id.home){
+            Toast.makeText(this, "action_home", Toast.LENGTH_SHORT).show();
+            replaceFragment(new HomeFragment());
+            return true;
+        }
+        return false;
+    }
+
 
 
 
